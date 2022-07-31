76b1caca540863330ef7baed551a8449cb686093
Damla Osman <damlaaosman@gmail.com>
2022-07-31 21:39:40 +1000

    Done.

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,19 @@
 // your ImageSlider code here!
+import { render } from "enzyme";
+import React, { Component } from 'react';
+
+export default class imageSlider extends Component {
+    constructor() {
+        super()
+        this.state = {
+            currentSlideIndex: 0
+        }
+    }
+
+    render() {
+        return (
+            <div>I am on slide {this.state.currentSlideIndex}</div>
+        )
+    }
+}
+
